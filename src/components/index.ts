5eaebde5fdcb4270d132cf10989bc0a2e9323d0d
Sergei Olshanetski <solshanetski@proofpoint.com>
2025-09-08 18:02:45 -0400

    Add missing file

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { NBATop20ScoringYouTube } from './NBATop20ScoringYouTube';
+export { NBATop20ScoringShortsPart1 } from './NBATop20ScoringShortsPart1';
+export { NBATop20ScoringShortsPart2 } from './NBATop20ScoringShortsPart2';
+export { NBATop20ScoringSquare } from './NBATop20ScoringSquare';
+export { NBATop20ScoringThumbnail } from './NBATop20ScoringThumbnail';
+export { CountdownSection } from './CountdownSection';
+export { PlayerCard } from './PlayerCard';
